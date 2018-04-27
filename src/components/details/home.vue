2cca5dda4896d654cbdc0a742bb091ec5d7ed9fe
wzj <jay_wzj@163.com>
2018-04-27 21:18:44 +0800

    1

--- src/components/details/home.vue
+++ src/components/details/home.vue
@@ -1,49 +1,51 @@
 <template>
-<van-pull-refresh  v-model="isLoading" @refresh="onRefresh">
-  <div   class="home_wrap">
-    <img src="@/assets/banner.jpg" alt="" class="home_wrap_banner">
-    <!-- 有订单时 -->
-    <div v-if="showList" style="padding-bottom: 1.7rem">
-      <div class="home_wrap_item" v-for="item in homeList" :key="item.id">
-        <router-link :to="{path: '/details/', query: { id: item.id }}">
-          <div class="time">下单时间：{{item.createDatePage}}<span :class="item.statusClass">{{item.statusPage}}</span></div>
-          <div class="content">
-            <img :src="item.category.icon" alt="" class="pic">
-            <div>
-              <div class="name">{{item.category.name}}</div>
-              <div class="price">预估价格：<span>￥{{item.price}}</span></div>
+  <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+    <div class="home_wrap">
+      <img src="@/assets/banner.jpg" alt="" class="home_wrap_banner">
+      <!-- 有订单时 -->
+      <div v-if="showList" style="padding-bottom: 1.7rem">
+        <div class="home_wrap_item" v-for="item in homeList" :key="item.id">
+          <router-link :to="{path: '/details/', query: { id: item.id }}">
+            <div class="time">下单时间：{{item.createDatePage}}<span :class="item.statusClass">{{item.statusPage}}</span>
             </div>
-          </div>
+            <div class="content">
+              <img :src="item.category.icon" alt="" class="pic">
+              <div>
+                <div class="name">{{item.category.name}}</div>
+                <div class="price">预估价格：<span>￥{{item.price}}</span></div>
+              </div>
+            </div>
+          </router-link>
+        </div>
+      </div>
+      <!-- 无订单时 -->
+      <div v-else>
+        <img src="@/assets/recycling.png" alt="" class="home_wrap_icon">
+        <div class="home_wrap_ordername">您还没有进行中的收呗订单</div>
+        <div class="home_wrap_btnname">点击下方立即回收按钮，为绿色生活助力吧！</div>
+      </div>
+      <!-- 底部footer -->
+      <div class="home_wrap_footer">
+        <img src="@/assets/icon_footer.png" alt="" class="home_wrap_footer_tbg">
+        <router-link to="/classIfy">
+          <div class="footer_circle">立即<br/>回收</div>
+        </router-link>
+        <router-link to="/records">
+          <div class="footer_left"><img src="@/assets/icon_foot.png" alt="" class="icon"><span>收呗足迹</span></div>
+        </router-link>
+        <router-link to="/orders">
+          <div class="footer_right"><img src="@/assets/icon_record.png" alt="" class="icon"><span>订单记录</span></div>
         </router-link>
       </div>
     </div>
-    <!-- 无订单时 -->
-    <div v-else>
-      <img src="@/assets/recycling.png" alt="" class="home_wrap_icon">
-      <div class="home_wrap_ordername">您还没有进行中的收呗订单</div>
-      <div class="home_wrap_btnname">点击下方立即回收按钮，为绿色生活助力吧！</div>
-    </div>
-    <!-- 底部footer -->
-    <div class="home_wrap_footer">
-      <img src="@/assets/icon_footer.png" alt="" class="home_wrap_footer_tbg">
-      <router-link to="/classIfy">
-        <div class="footer_circle">立即<br/>回收</div>
-      </router-link>
-      <router-link to="/records">
-        <div class="footer_left"><img src="@/assets/icon_foot.png" alt="" class="icon"><span>收呗足迹</span></div>
-      </router-link>
-      <router-link to="/orders">
-        <div class="footer_right"><img src="@/assets/icon_record.png" alt="" class="icon"><span>订单记录</span></div>
-      </router-link>
-    </div>
-  </div>
-</van-pull-refresh>
+  </van-pull-refresh>
 </template>
 <script>
   import '@/assets/detailstyle/home.css'
   import api from '@/api/api.js'
   import {mapGetters} from 'vuex';
   import {PullRefresh} from 'vue-ydui/dist/lib.rem/pullrefresh';
+
   export default {
     name: "home",
     data() {
@@ -51,7 +53,7 @@
         homeList: {},
         showList: true,
         count: 0,
-        isLoading: false
+        isLoading: false,
       }
     },
     created() {
@@ -59,7 +61,7 @@
       var tk = urlToken.split("=");
       var testToken = "3F3TEMH74565Q5QORHNPE76UZM6VT4JPWVV4OPUNTGAXLLRLC6B5GYU3LW34YHVNOEFL2LXPVT24UCSPFZ54XJGBWOYJ2KXG2OVQSA4XXBTPXSV2LCS2ZJHIRFEZICJPZDF3ZEBP3BYPWXK2Z5KX5VOOLC6EQXK3DHR3CPY43MKE3HLRVQFBVFFSBL2D734IUX7SLAVFXSWJ5XSRKODFUYOD23YECXXDBJ4WQHGD3SFWEON7HRT5LZ3OS75EKML5UDKQC76XR6NNRF6K4IRHCEA6NZSB7NRXZVRFSC3PZ4O2ZIFJROTQ"
       this.$store.dispatch('getToken', testToken)
-      // this.$store.dispatch('getToken',tk[1])
+      // this.$store.dispatch('getToken', tk[1])
     },
     computed: mapGetters({
       token: "token"
@@ -71,7 +73,7 @@
       this.getData();
     },
     methods: {
-      onRefresh(){
+      onRefresh() {
         //获取数据
         api.getHome({
           "app_key": "app_id_1",
@@ -81,7 +83,6 @@
           this.$toast('刷新成功');
           this.isLoading = false;
           this.count++;
-
           console.log(res.data);
           if (res.data.length === 0) {
             this.showList = false;
@@ -146,9 +147,28 @@
   }
 </script>
 <style>
-.van-pull-refresh{
-  width: 100%;
-}
+  .van-pull-refresh {
+    width: 100%;
+
+  }
+
+  .van-toast {
+    font-size: 0.3rem;
+    line-height: 0.012rem;
+    border-radius: 0.1rem;
+  }
+
+  .van-toast--text {
+    padding: 0.6rem;
+    min-width: 1.6rem;
+  }
+
+  .van-pull-refresh__head {
+    height: 0.5rem;
+    top: -0.5rem;
+    font-size: 0.24rem;
+    line-height: 0.5rem;
+  }
 </style>
 
 
