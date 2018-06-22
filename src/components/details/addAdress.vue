b014d170d547fe3956ad07c8ba47a1c6f1f6331b
miaojing <1336619526@qq.com>
2018-06-22 17:37:15 +0800

    1

--- src/components/details/addAdress.vue
+++ src/components/details/addAdress.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="add_wrap">
-    123
+    <div class="add_item">
+      <div class="title">
+        <div class="left">联系人</div>
+        <div class="right"><input type="text" placeholder="请输入姓名"></div>
+      </div>
+      <div class="title">
+        <div class="left">手机号码</div>
+        <div class="right"><input type="text" placeholder="请输入手机号"></div>
+      </div>
+      <div class="title">
+        <div class="left">所在地址</div>
+        <div class="right">请选择所在地区<img src="@/assets/icon_right.png" alt=""></div>
+      </div>
+      <div class="title">
+        <div class="left">详细地址</div>
+        <div class="right"><input type="text" placeholder="请输入街道、楼牌号等"></div>
+      </div>
+    </div>
+    <div class="add_btn">保存</div>
   </div>
 </template>
 
