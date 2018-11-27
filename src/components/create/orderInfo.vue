599a0e10c4122c3e7ba84dfbb17ebeb4d937a058
PeterYueYue <51506001@qq.com>
2018-11-27 17:04:42 +0800

    1

--- src/components/create/orderInfo.vue
+++ src/components/create/orderInfo.vue
@@ -80,7 +80,7 @@
       <div class="title">回收小贴士</div>
       <div class="remind">回收规则升级啦！您还可以将回收物送给辛苦上门的废品大叔，按回收物公斤重量换取绿色环保能量，在积分商城兑换您的权益。</div>
       <div class="btn nocolor" @click="completeAnOrder('0')">我要卖钱</div>
-      <div class="btn" @click="completeAnOrder('1')">打包带走，获取双倍环保能量</div>
+      <div class="btn" @click="completeAnOrder('1')">一起带走，换取积分</div>
     </div>
 
   </div>
